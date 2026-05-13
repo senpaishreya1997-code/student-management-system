bef0dd6696a53233fa0e59ad3725bbb0c60b007c
senpaishreya1997-code <senpaishreya1997@gmail.com>
2026-05-13 23:35:48 +0530

    Modify DBConnection with new database credentials
    Updated the database connection details for MySQL.

--- UniversityERP/src/main/java/com/university/dao/DBConnection.java
+++ UniversityERP/src/main/java/com/university/dao/DBConnection.java
@@ -1,1 +1,24 @@
+package com.university.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL =
+        "jdbc:mysql://localhost:3306/university_erp?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root"; // ← change this
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("MySQL Driver not found", e);
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
